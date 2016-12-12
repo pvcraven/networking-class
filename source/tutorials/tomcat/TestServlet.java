d43a58d4d8347019fa9f9e1f7285e8dcefe67b9b
Paul Vincent Craven <paul@cravenfamily.com>
2016-12-11 21:54:20 -0600

    Update

---
+++ source/tutorials/tomcat/TestServlet.java
@@ -0,0 +1,32 @@
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class TestServlet extends HttpServlet {
+
+  private String message;
+
+  public void init() throws ServletException
+  {
+      // Do required initialization
+  }
+
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException
+  {
+      // Set response content type
+      response.setContentType("text/html");
+
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<html><h1>Hello World</h1></html>");
+  }
+
+  public void destroy()
+  {
+      // do nothing.
+  }
+}
